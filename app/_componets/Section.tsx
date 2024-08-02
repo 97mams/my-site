d2d7ff403f8db66ef16ee4a3d7fd45f52e3f2302
97mams <97mams@gmail.com>
2024-08-02 08:34:03 +0300

    feat: work card, contact me

--- app/_componets/Section.tsx
+++ app/_componets/Section.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react"
 import { cn } from "@/lib/utils"
 export const Section = (props: PropsWithChildren<{ className?: string }>) => {
     return (
-        <section className={cn("max-w-3xl px-4 m-auto", props.className)}>
+        <section className={cn("max-w-4xl px-4 m-auto", props.className)}>
             {props.children}
         </section>
     )
--- app/_componets/Status.tsx
+++ app/_componets/Status.tsx
@@ -3,11 +3,12 @@
 import Link from "next/link"
 import { Section } from "./Section"
 import { Card } from "@/components/ui/card"
-import { Code, Home, LucideIcon, Notebook, Share, StickyNote, Weight } from "lucide-react"
+import { Code, Home, LucideIcon, ArrowUpRight, Share, StickyNote, Weight } from "lucide-react"
+import Image from "next/image"
 
 export const Status = () => {
     return (
-        <Section className="flex max-md:flex-col gap-4 items-start">
+        <Section className="flex max-md:flex-col gap-2 items-start">
             <div className="flex-[3] w-full">
                 <Card className="p-4 flex flex-col gap-2 ">
                     <p className="text-lg text-muted-foreground">Side, fun projects</p>
@@ -22,23 +23,75 @@ export const Status = () => {
                     ))}
                 </Card>
             </div>
-            <div className="flex-[2] w-full gap-2">
-                <Card className="flex-1 p-4 w-full">
-                    {WORKS.map((work, index) => (
-                        <Work
-                            key={index}
-                            Image={work.image}
-                            title={work.title}
-                            role={work.role}
-                            date={work.date}
-                        />
-                    ))}
+            <div className="flex-[2] flex flex-col w-full gap-2">
+                <Card className="flex-1 p-4 ">
+                    <p className="text-lg text-muted-foreground">Work</p>
+                    <div className="flex flex-col gap-4">
+                        {WORKS.map((work, index) => (
+                            <Work
+                                key={index}
+                                Image={work.image}
+                                title={work.title}
+                                role={work.role}
+                                date={work.date}
+                            />
+                        ))}
+                    </div>
                 </Card>
-                <Card className="flex-1 p-4 w-full">
-                    contact me
+                <Card className="flex flex-col flex-1 p-4 w-full gap-2">
+                    <p className="text-lg text-muted-foreground">contact me</p>
+                    <ContactCard
+                        image="/saryko.png"
+                        mediumImage=""
+                        name="97mams"
+                        description="5"
+                    />
+                    <ContactCard
+                        image="/saryko.png"
+                        mediumImage=""
+                        name="97mams"
+                        description="5"
+                    />
                 </Card>
             </div>
         </Section >
+    )
+}
+
+const ContactCard = (props: {
+    image: string
+    mediumImage: string
+    name: string
+    description: string
+}) => {
+    return (
+        <Card className="p-3 bg-accent/10 flex items-center gap-4 hover:bg-accent/30 transition-colors group">
+            <div className="relative">
+                <Image
+                    src={props.image}
+                    height={40}
+                    width={40}
+                    alt={props.name}
+                    className="rounded-full object-contain"
+                />
+                <Image
+                    src={props.mediumImage}
+                    height={16}
+                    width={16}
+                    alt={props.name}
+                    className="absolute -bottom-2 -right-2 rounded-full"
+                />
+            </div>
+            <div className="mr-auto">
+                <p className="text-lg font-semibold">
+                    {props.name}
+                </p>
+                <p className="text-sm text-muted-forgroud">
+                    {props.description}
+                </p>
+            </div>
+            <ArrowUpRight className="group-hover:translate-x-2 group-hover:-translate-y-2 mr-4" size={16} />
+        </Card>
     )
 }
 
@@ -47,6 +100,18 @@ const SIDE_PROJECT = [
         Logo: Code,
         title: "Calculator",
         description: "a project to calculate in the terminal, create with php",
+        url: "https://github.com"
+    },
+    {
+        Logo: Code,
+        title: "Authe",
+        description: "a project for loing account and hash password, create with php",
+        url: "https://github.com"
+    },
+    {
+        Logo: Weight,
+        title: "Mon Agens",
+        description: "project to practice Laravel",
         url: "https://github.com"
     },
     {
@@ -89,15 +154,15 @@ const SideFunProject = (props: sideProjectProps) => {
 const WORKS = [
     {
         image: "/",
-        title: "Paositra",
+        title: "Paositra Mg",
         role: "Stagiaire",
-        date: "Septembre - Décembre 2023"
+        date: "Sep-Déc 2023"
     },
     {
         image: "/",
-        title: "R@ndev",
+        title: "R@ndevTeam",
         role: "Stagiaire",
-        date: "Septembre - Décembre 2022"
+        date: "Sep-Déc 2022"
     },
 
 ]
@@ -110,25 +175,29 @@ type WorkProps = {
 }
 
 const Work = (props: WorkProps) => {
-    <div className="inline-flex items-center gap-4 hover:bg-accent/50 rounded">
-        <span className="bg-accent text-accent-foregroud ">
-            <img
-                src={props.Image}
-                alt={props.title}
-                className="w-4 h-4 object-contain"
-            />
-        </span>
-        <p className="text-lg font-semibold">
-            {props.title}
-        </p>
-        <span>
-            <p className="text-sm text-muted-forgroud">
-                {props.role}
-            </p>
-            <p className="ml-auto">
-                {props.date}
-            </p>
-        </span>
-    </div>
+    return (
+        <div className="inline-flex items-center gap-4 hover:bg-accent/50 transition-colors p-1 rounded">
+            <span className="bg-accent text-accent-foregroud ">
+                <img
+                    src={props.Image}
+                    alt={props.title}
+                    className="w-4 h-4 object-contain"
+                />
+            </span>
+            <div className="mr-auto">
+                <p className="text-lg font-semibold">
+                    {props.title}
+                </p>
+                <p className="text-sm text-muted-forgroud">
+                    {props.role}
+                </p>
+            </div>
+            <div>
+                <p className="text-sm text-end text-muted-foreground">
+                    {props.date}
+                </p>
+            </div>
+        </div>
+    )
 
 }
