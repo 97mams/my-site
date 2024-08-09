775eae968bb9e8f0596a1c77b7b52847d3fbf95a
97mams <97mams@gmail.com>
2024-08-09 20:50:40 +0300

    feat: code

---
+++ app/_componets/Code.tsx
@@ -0,0 +1,13 @@
+import { ComponentPropsWithoutRef } from "react"
+import { cn } from "@/lib/utils"
+
+export const Code = ({ className, ...props }: ComponentPropsWithoutRef<"span">) => {
+    return (
+        <span
+            className={cn("bg-accent/30 font-mono border border-accent p-1 text-primary rounded-sm",
+                className
+            )}
+            {...props}
+        />
+    )
+}
