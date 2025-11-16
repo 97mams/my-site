6040813c9d5811df93b9676d260982279948527c
97mams <97mams@gmail.com>
2025-11-16 10:42:43 +0300

    refactor: reorder imports and improve formatting in Header and Section components

--- app/_componets/Section.tsx
+++ app/_componets/Section.tsx
@@ -1,9 +1,9 @@
-import { PropsWithChildren } from "react"
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { PropsWithChildren } from "react";
 export const Section = (props: PropsWithChildren<{ className?: string }>) => {
-    return (
-        <section className={cn("max-w-4xl px-4 m-auto", props.className)}>
-            {props.children}
-        </section>
-    )
-}
+  return (
+    <div className={cn("max-w-4xl px-4 m-auto", props.className)}>
+      {props.children}
+    </div>
+  );
+};
