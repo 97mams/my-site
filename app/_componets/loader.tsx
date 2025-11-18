d9b0a6f57ab4d29dcfa398820e453e5db8f2e9b0
97mams <97mams@gmail.com>
2025-11-18 17:27:56 +0300

    feat: add Loader component with skeleton placeholders

---
+++ app/_componets/loader.tsx
@@ -0,0 +1,36 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function Loader() {
+  return (
+    <div className="w-full flex flex-col gap-2">
+      <div className="inline-flex items-center gap-4 transition-colors p-1 rounded">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="inline-flex items-center gap-4 transition-colors p-1 rounded">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="inline-flex items-center gap-4 transition-colors p-1 rounded">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="inline-flex items-center gap-4 transition-colors p-1 rounded">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+    </div>
+  );
+}
